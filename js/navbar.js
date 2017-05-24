9c48761792bd874a9dcc0535e4567962f4d7ad2a
Badaptos <alex.g9415@gmail.com>
2017-05-24 11:17:24 -0700

    changed a few lines in navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,5 @@
-var mobileNav = document.querySelector('.mobile-nav');
 var menuIcon = document.querySelector('.navbar-menu-icon');
 menuIcon.addEventListener('click', function(){
+  var mobileNav = document.querySelector('.mobile-nav');
   mobileNav.classList.toggle('openNav');
 });
