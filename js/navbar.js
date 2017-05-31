7b5635dac329fa03abe866c6a1fff936e247eedc
Badaptos <alex.g9415@gmail.com>
2017-05-31 09:48:13 -0700

    made nav for mobile and desktop. working on js interactivity

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,13 @@
 var menuIcon = document.querySelector('.navbar-menu-icon');
 menuIcon.addEventListener('click', function(){
-  var mobileNav = document.querySelector('.mobile-nav');
-  mobileNav.classList.toggle('openNav');
+  var mobileNav = document.querySelector('.mobile-nav');;
+  if(mobileNav.classList === ''){
+    mobileNav.classList.add('openNav');
+  } else if (mobileNav.classList == 'openNav') {
+    mobileNav.classList.remove('openNav');
+    mobileNav.classList.add('closeNav');
+  }
 });
+
+// when burger icon is clicked nav opens
+// when burger icon is clicked again nav closes
