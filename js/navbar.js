a72810bf647875b1cab162b7bd93adad11329f3a
Badaptos <alex.g9415@gmail.com>
2017-05-31 16:32:18 -0700

    Slide out navigation is working. removed image from billboard and left a green background.

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,6 @@
 var menuIcon = document.querySelector('.navbar-menu-icon');
-var mobileNav = document.querySelector('.mobile-nav');
 
 menuIcon.addEventListener('click', function(){
+  var mobileNav = document.querySelector('.mobile-nav');
   mobileNav.classList.toggle('openNav');
 });
