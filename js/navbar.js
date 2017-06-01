05fd807958ea464c5713ba7b93b686d4d588d84c
Badaptos <alex.g9415@gmail.com>
2017-05-31 17:21:04 -0700

    Created animation for hamburger menu button. makes it into an x when menu is open

--- js/navbar.js
+++ js/navbar.js
@@ -3,4 +3,5 @@ var menuIcon = document.querySelector('.navbar-menu-icon');
 menuIcon.addEventListener('click', function(){
   var mobileNav = document.querySelector('.mobile-nav');
   mobileNav.classList.toggle('openNav');
+  menuIcon.classList.toggle('x');
 });
