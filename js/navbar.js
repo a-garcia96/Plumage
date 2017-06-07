866e92d3a416902046bc391ffdff3e7d9ac3764d
Badaptos <alex.g9415@gmail.com>
2017-06-06 22:01:09 -0700

    made a single js file for plumage

--- js/navbar.js
+++
@@ -1,7 +0,0 @@
-var menuIcon = document.querySelector('.navbar-menu-icon');
-
-menuIcon.addEventListener('click', function(){
-  var mobileNav = document.querySelector('.nav-mobile-menu');
-  mobileNav.classList.toggle('openNav');
-  menuIcon.classList.toggle('x');
-});
